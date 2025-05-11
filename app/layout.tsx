9b001661b1c8462456c04734811f29f6f9662a79
Ellipsis <rizkyfathur326@gmail.com>
2025-05-11 16:42:37 +0700

    ADD icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Little Alchemy 2 Recipe Finder",
   description: "Find recipes in Little Alchemy 2 using BFS and DFS algorithms",
   icons: {
-    icon: "/favicon.ico",
+    icon: "/icon.png",
   },
 };
 
